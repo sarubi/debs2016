78a667bb6f30f0935ecc82191d0b0f5cbc431e6e
Malith Jayasinghe <malithj@wso2.com>
2016-03-06 18:43:22 +0530

    Fix the bug in the DataLoaderThread - it now reads the full file from start to end

--- src/main/java/org/wso2/siddhi/debs2016/input/DataLoaderThread.java
+++ src/main/java/org/wso2/siddhi/debs2016/input/DataLoaderThread.java
@@ -5,6 +5,7 @@ import com.google.common.base.Splitter;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -12,7 +13,7 @@ import java.util.concurrent.LinkedBlockingQueue;
  * Reads the data from the file and store it in memory
  * 
  */
-public class DataLoderThread extends Thread {
+public class DataLoaderThread extends Thread {
     private String fileName;
     private final static Splitter splitter = Splitter.on('|');
     private LinkedBlockingQueue<Object[]> eventBufferList;
@@ -28,7 +29,7 @@ public class DataLoderThread extends Thread {
      * @param eventBuffer the blocking queue which stores the data read from the file
      * @param fileType the type of the file to be read
      */
-    public DataLoderThread(String fileName, LinkedBlockingQueue<Object[]> eventBuffer, FileType fileType){
+    public DataLoaderThread(String fileName, LinkedBlockingQueue<Object[]> eventBuffer, FileType fileType){
         super("Data Loader");
         this.fileName = fileName;
         this.eventBufferList = eventBuffer;
@@ -39,22 +40,20 @@ public class DataLoderThread extends Thread {
         try {
             br = new BufferedReader(new FileReader(fileName), 10 * 1024 * 1024);
             String line = br.readLine();
-            Object[] eventData = null;
-            Long userID = -1l;
-            String user = null;
+            Object[] eventData;
+            String user ;
 
             while (line != null) {
-                //We make an assumption here that we do not get empty strings due to missing values that may present in the input data set.
+                //We make an assumption here that we do not get empty strings due to missing values that may present in the input data set
                 Iterator<String> dataStrIterator = splitter.split(line).iterator();
                 switch(fileType) {
                     case POSTS:
                         //ts long, post_id long, user_id long, post string, user string
                         String postsTimeStamp = dataStrIterator.next(); //e.g., 2010-02-01T05:12:32.921+0000
                         Long postID = Long.parseLong(dataStrIterator.next());
-                        userID = Long.parseLong(dataStrIterator.next());
+                        Long userID = Long.parseLong(dataStrIterator.next());
                         String post = dataStrIterator.next();
                         user = dataStrIterator.next();
-
                         eventData = new Object[]{
                                 0l,//We need to attach the time when we are injecting an event to the query network.
                                 // For that we have to set a separate field which will be populated when we are
@@ -65,13 +64,12 @@ public class DataLoderThread extends Thread {
                                 post,
                                 user
                         };
-
+                        //System.out.println(count++);
                         eventBufferList.put(eventData);
                         break;
                     case COMMENTS:
                         //ts long, comment_id long, user_id long, comment string, user string, comment_replied long,
                         // post_commented long
-
                         String commentTimeStamp = dataStrIterator.next(); //e.g., 2010-02-09T04:05:20.777+0000
                         Long commentID = Long.parseLong(dataStrIterator.next());
                         userID = Long.parseLong(dataStrIterator.next());
@@ -98,7 +96,6 @@ public class DataLoderThread extends Thread {
                                 comment_replied,
                                 post_commented
                         };
-
                         eventBufferList.put(eventData);
                         break;
                     case FRIENDSHIPS:
@@ -106,14 +103,18 @@ public class DataLoderThread extends Thread {
                     case LIKES:
                         break;
                 }
+                 line = br.readLine();
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (NumberFormatException ec){
             ec.printStackTrace();
-        } catch (Throwable e) {
+        } catch (IOException ec){
+            ec.printStackTrace();
+        } catch (InterruptedException e){
             e.printStackTrace();
         }
+
     }
 
     /**
