bcae741ab7059b50a10ea53c4c136051adaa251b
Malith Jayasinghe <malithj@wso2.com>
2016-03-08 13:51:29 +0530

    adding a test class for FriendshipGraph

---
+++ src/test/java/FriendshipGraphTest.java
@@ -0,0 +1,53 @@
+import org.junit.Test;
+import org.wso2.siddhi.debs2016.graph.FriendshipGraph;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by malithjayasinghe on 3/8/16.
+ */
+public class FriendshipGraphTest {
+
+
+    @Test
+    public void testGraphConstructionDataSet1()
+    {
+       // FriendshipGraph graph = new FriendshipGraph(null);
+        // edges = graph.getNumberOfEdges();
+
+
+        //assertEquals(3,edges);
+
+    }
+
+    public void testGraphConstructionDataSet2()
+    {
+
+
+    }
+
+    public void testAddEdge()
+    {
+
+
+    }
+
+
+    public void testGetConnectedComponents()
+    {
+
+
+    }
+
+    public void getLargestConnectedComponent()
+    {
+
+
+    }
+
+
+
+
+
+
+}
