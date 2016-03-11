eb8296c370d4c69fdf62841ffd0827e22e856b0c
Malith Jayasinghe <malithj@wso2.com>
2016-03-11 14:17:58 +0530

    handleNewFriendship() should be public

--- src/main/java/org/wso2/siddhi/debs2016/comment/CommentStore.java
+++ src/main/java/org/wso2/siddhi/debs2016/comment/CommentStore.java
@@ -101,7 +101,7 @@ public class CommentStore {
      * @param uId1 the userID of friend one
      * @param uId2 the userID of friend two
      */
-    private void handleNewFriendship(long uId1, long uId2)
+    public void handleNewFriendship(long uId1, long uId2)
     {
         for (CommentLikeGraph commentLikeGraph: graph.values()) {
             commentLikeGraph.handleNewFriendship(uId1, uId2);
