4cf3eeadd6ce63bcc4e13beabee696e521383ed1
Malith Jayasinghe <malithj@wso2.com>
2016-04-06 14:32:51 +0530

    Merging queries: implemented two different ordered event senders for q1 and q2

---
+++ src/main/java/org/wso2/siddhi/debs2016/sender/OrderedEventSenderThreadQ1.java
@@ -0,0 +1,159 @@
+package org.wso2.siddhi.debs2016.sender;
+
+import org.wso2.siddhi.core.stream.input.InputHandler;
+import org.wso2.siddhi.debs2016.util.Constants;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * The event sender for query 1
+ *
+ */
+public class OrderedEventSenderThreadQ1 extends Thread {
+
+    private LinkedBlockingQueue<Object[]> eventBufferList[];
+    private InputHandler inputHandler;
+    private Date startDateTime;
+    public boolean doneFlag = false;
+
+    /**
+     * The constructor
+     *
+     * @param eventBuffer  the event buffer array
+     * @param inputHandler the input handler array
+     */
+    public OrderedEventSenderThreadQ1(LinkedBlockingQueue<Object[]> eventBuffer[], InputHandler inputHandler) {
+        super("Event Sender");
+        this.eventBufferList = eventBuffer;
+        this.inputHandler = inputHandler;
+    }
+
+
+    public void run() {
+        Object[] commentEvent = null;
+        Object[] postEvent = null;
+
+        long count = 1;
+        long timeDifferenceFromStart = 0;
+        long timeDifference = 0; //This is the time difference for this time window.
+        long currentTime = 0;
+        long prevTime = 0;
+        //long startTime = System.currentTimeMillis();
+        long startTime = 0;
+        long cTime = 0;
+        //Special note : Originally we need not subtract 1. However, due to some reason if there are n events in the input data set that are
+        //pumped to the eventBufferList queue, only (n-1) is read. Therefore, we have -1 here.
+        //final int EVENT_COUNT = Integer.parseInt(Config.getConfigurationInfo("org.wso2.siddhi.debs2015.dataset.size")) - 1;
+
+        boolean firstEvent = true;
+        float percentageCompleted = 0;
+        int flag = Constants.NOEVENT;
+
+        while (true) {
+
+            try {
+                //Send dummy event to mark the commencement of processing
+                if (firstEvent) {
+                    Object[] firstPostEvent = new Object[]{
+                            0L,
+                            -1L,
+                            0L,
+                            0L,
+                            "",
+                            "",
+                            0L,
+                            0L,
+                            Constants.POSTS
+                    };
+                    cTime = System.currentTimeMillis();
+                    firstPostEvent[Constants.INPUT_INJECTION_TIMESTAMP_FIELD] = cTime;
+                    inputHandler.send(cTime, firstPostEvent);
+
+                    //We print the start and the end times of the experiment even if the performance logging is disabled.
+                    startDateTime = new Date();
+                    startTime = startDateTime.getTime();
+                    SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd.hh:mm:ss-a-zzz");
+                    System.out.println("Started experiment at : " + startTime + "--" + ft.format(startDateTime));
+                    firstEvent = false;
+                }
+
+                try {
+
+                    if (flag == Constants.POSTS) {
+                        postEvent = eventBufferList[Constants.POSTS].poll(1000, TimeUnit.MILLISECONDS);
+                    } else if (flag == Constants.COMMENTS) {
+                        commentEvent = eventBufferList[Constants.COMMENTS].poll(1000, TimeUnit.MILLISECONDS);
+                    } else {
+                        postEvent = eventBufferList[Constants.POSTS].poll(1000, TimeUnit.MILLISECONDS);
+                        commentEvent = eventBufferList[Constants.COMMENTS].poll(1000, TimeUnit.MILLISECONDS);
+                    }
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
+                long tsComment;
+                long tsPost;
+
+                //handling the instance where the stream of a buffer has no more events
+                if (commentEvent == null) {
+                    tsComment = Long.MAX_VALUE;
+                } else {
+                    tsComment = (Long) commentEvent[Constants.EVENT_TIMESTAMP_FIELD];
+                }
+
+                if (postEvent == null) {
+                    tsPost = Long.MAX_VALUE;
+                } else {
+                    tsPost = (Long) postEvent[Constants.EVENT_TIMESTAMP_FIELD];
+                }
+
+                if (tsComment < tsPost && tsComment != Long.MAX_VALUE) {
+                    cTime = System.currentTimeMillis();
+                    commentEvent[Constants.INPUT_INJECTION_TIMESTAMP_FIELD] = cTime; //This corresponds to the iij_timestamp
+                    inputHandler.send(cTime, commentEvent);
+                    flag = Constants.COMMENTS;
+                } else if (tsPost != Long.MAX_VALUE) {
+                    cTime = System.currentTimeMillis();
+                    postEvent[Constants.INPUT_INJECTION_TIMESTAMP_FIELD] = cTime; //This corresponds to the iij_timestamp
+                    inputHandler.send(cTime, postEvent);
+                    flag = Constants.POSTS;
+                }
+
+                count++;
+
+                //When all buffers are empty
+                if (commentEvent == null && postEvent == null) {
+                    //Sending second dummy event to signal end of streams
+                    cTime = System.currentTimeMillis();
+
+                    Object[] finalPostEvent = new Object[]{
+                            0L,
+                            -2L,
+                            0L,
+                            0L,
+                            "",
+                            "",
+                            0L,
+                            0L,
+                            Constants.POSTS
+                    };
+
+                    finalPostEvent[Constants.INPUT_INJECTION_TIMESTAMP_FIELD] = cTime;
+                    Thread.sleep(1000);//We just sleep for short period so that we can ensure that all the data events have been processed by the ranker properly before we shutdown.
+                    inputHandler.send(cTime, finalPostEvent);
+
+                    doneFlag = true;
+                    break;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
---
+++ src/main/java/org/wso2/siddhi/debs2016/sender/OrderedEventSenderThreadQ2.java
@@ -0,0 +1,187 @@
+package org.wso2.siddhi.debs2016.sender;
+
+/**
+ * Created by malithjayasinghe on 4/6/16.
+ */
+
+import org.wso2.siddhi.core.stream.input.InputHandler;
+import org.wso2.siddhi.debs2016.Processors.DEBSEvent;
+import org.wso2.siddhi.debs2016.Processors.DEBSEventManager;
+import org.wso2.siddhi.debs2016.util.Constants;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+
+import org.wso2.siddhi.core.stream.input.InputHandler;
+import org.wso2.siddhi.debs2016.Processors.DEBSEvent;
+import org.wso2.siddhi.debs2016.Processors.DEBSEventManager;
+import org.wso2.siddhi.debs2016.util.Constants;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * The event sender for query 2
+ *
+ * Created by anoukh on 3/15/16.
+ */
+public class OrderedEventSenderThreadQ2 extends Thread {
+
+    private LinkedBlockingQueue<Object[]> eventBufferList [];
+    private InputHandler inputHandler;
+    private Date startDateTime;
+    public boolean doneFlag = false;
+    DEBSEventManager manager = new DEBSEventManager();
+
+
+    /**
+     * The constructor
+     *
+     * @param eventBuffer the event buffer array
+     * @param inputHandler the input handler array
+     */
+    public OrderedEventSenderThreadQ2(LinkedBlockingQueue<Object[]> eventBuffer [], InputHandler inputHandler) {
+        super("Event Sender");
+        this.eventBufferList = eventBuffer;
+        this.inputHandler = inputHandler;
+        manager.run();
+    }
+
+    /**
+     *
+     * Start the data reader thread
+     *
+     */
+    public void run(){
+        Object[] friendshipEvent = null;
+        Object[] commentEvent = null;
+        Object[] likeEvent = null;
+        long startTime = 0;
+        long cTime = 0;
+        boolean firstEvent = true;
+        int flag = Constants.NOEVENT;
+
+        while(true){
+            //Send dummy event to mark the commencement of processing
+            if(firstEvent){
+                Object[] finalFriendshipEvent = new Object[]{
+                        0L,
+                        -1L,
+                        0L,
+                        0L,
+                        0L,
+                        0L,
+                        0L,
+                        0L,
+                        0,
+                };
+                cTime = System.currentTimeMillis();
+                DEBSEvent event = manager.getNextDebsEvent();
+                event.setObjectArray(finalFriendshipEvent);
+                event.setSystemArrivalTime(cTime);
+                manager.publish();
+
+                //We print the start and the end times of the experiment even if the performance logging is disabled.
+                startDateTime = new Date();
+                startTime = startDateTime.getTime();
+                SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd.hh:mm:ss-a-zzz");
+                System.out.println("Started experiment at : " + startTime + "--" + ft.format(startDateTime));
+                firstEvent = false;
+            }
+
+            try{
+                if (flag == Constants.FRIENDSHIPS){
+                    friendshipEvent = eventBufferList[Constants.FRIENDSHIPS].poll(500, TimeUnit.MILLISECONDS);
+                }else if (flag == 1){
+                    commentEvent = eventBufferList[Constants.COMMENTS].poll(500, TimeUnit.MILLISECONDS);
+                }else if (flag == 2){
+                    likeEvent = eventBufferList[Constants.LIKES].poll(500, TimeUnit.MILLISECONDS);
+                }else{
+                    friendshipEvent = eventBufferList[Constants.FRIENDSHIPS].take();
+                    commentEvent = eventBufferList[Constants.COMMENTS].take();
+                    likeEvent = eventBufferList[Constants.LIKES].take();
+                }
+
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+
+            long tsFriendship;
+            long tsComment;
+            long tsLike;
+
+            //handling the instance where the stream of a buffer has no more events
+            if (friendshipEvent == null){
+                tsFriendship = Long.MAX_VALUE;
+            }else{
+                tsFriendship = (Long) friendshipEvent[Constants.EVENT_TIMESTAMP_FIELD];
+            }
+
+            if (commentEvent == null){
+                tsComment = Long.MAX_VALUE;
+            }else{
+                tsComment = (Long) commentEvent[Constants.EVENT_TIMESTAMP_FIELD];
+            }
+
+            if (likeEvent == null){
+                tsLike = Long.MAX_VALUE;
+            }else{
+                tsLike = (Long) likeEvent[Constants.EVENT_TIMESTAMP_FIELD];
+            }
+
+            if (tsFriendship <= tsComment && tsFriendship <= tsLike && tsFriendship != Long.MAX_VALUE){
+                cTime = System.currentTimeMillis();
+                DEBSEvent debsEvent = manager.getNextDebsEvent();
+                debsEvent.setObjectArray(friendshipEvent);
+                debsEvent.setSystemArrivalTime(cTime);
+                manager.publish();
+                flag = Constants.FRIENDSHIPS;
+            }else if (tsComment <= tsFriendship && tsComment <= tsLike && tsComment != Long.MAX_VALUE){
+                cTime = System.currentTimeMillis();
+                DEBSEvent debsEvent = manager.getNextDebsEvent();
+                debsEvent.setObjectArray(commentEvent);
+                debsEvent.setSystemArrivalTime(cTime);
+                manager.publish();
+                flag = Constants.COMMENTS;
+            }else if (tsLike != Long.MAX_VALUE){
+                cTime = System.currentTimeMillis();
+                DEBSEvent debsEvent = manager.getNextDebsEvent();
+                debsEvent.setObjectArray(likeEvent);
+                debsEvent.setSystemArrivalTime(cTime);
+                manager.publish();
+                flag = Constants.LIKES;
+            }
+
+            //When all buffers are empty
+            if (friendshipEvent == null && commentEvent == null && likeEvent == null){
+                cTime = System.currentTimeMillis();
+                //Send dummy event to signal end of all streams
+                Object[] finalFriendshipEvent = new Object[]{
+                        0L,
+                        -2L,
+                        0L,
+                        0L,
+                        0L,
+                        0L,
+                        0L,
+                        0L,
+                        0,
+                };
+
+                DEBSEvent debsEvent = manager.getNextDebsEvent();
+                debsEvent.setObjectArray(finalFriendshipEvent);
+                debsEvent.setSystemArrivalTime(cTime);
+                manager.publish();
+                manager.getDataReadDisruptor().shutdown();
+                doneFlag = true;
+                break;
+            }
+        }
+
+    }
+}
